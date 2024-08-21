ed1d26d44cfe4940bb8f2500f9e81412540840dc
Andre-r1 <andrexpsouza@gmail.com>
2024-08-21 15:02:07 +0000

    praticas02-

--- aula/praticas/praticas02/imprima_boletim.c
+++ aula/praticas/praticas02/imprima_boletim.c
@@ -1,6 +1,11 @@
-# include <stdio.h>
+# include <stdio.h>//biblioteca math
+#include <math.h>
 int main(){
-  printf("%f Matricula A1 A2 Media");
+  printf("%s\t\n, Matricula A1 A2 Media");
+  printf("%i\t%3.1f\t%3.1f\t%3.1f\n", 20210010, 8.5, 5.0, 6.4);
+   printf("%i\t%3.1f\t%3.1f\t%3.1f\n", 20215053, 4.7, 10.0,7.9);
+   printf("%i\t%3.1f\t%3.1f\t%3.1f\n", 20210010, 8.5, 5.0, 6.4);
+   printf("%i\t%3.1f\t%3.1f\t%3.1f\n", 20210010, 8.5, 5.0, 6.4);
   
   return 0;
 }
