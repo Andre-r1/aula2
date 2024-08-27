34431316184577ec916415c878145e9b6d4000cf
Andre-r1 <andrexpsouza@gmail.com>
2024-08-27 14:33:59 +0000

    quasela

--- aula/praticas/praticas02/imprima_nota_legal.c
+++ aula/praticas/praticas02/imprima_nota_legal.c
@@ -3,6 +3,9 @@
 int main(){
   printf("\n\n=============================\n\n");
   printf("      N O T A  L E G A L      \n");
+  printf("\n=============================\n\n");
+printf("%sProduto         Qtd Valor Unit""\n");
+  printf("%sCamiseta %f2 %i39.90\n");  
   
   return 0;
 }
