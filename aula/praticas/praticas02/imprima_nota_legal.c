105bb7e2a2a6c19c61a8c51917e65361fe6c0d2a
Andre-r1 <andrexpsouza@gmail.com>
2024-08-27 23:44:41 +0000

    pronto.

--- aula/praticas/praticas02/imprima_nota_legal.c
+++ aula/praticas/praticas02/imprima_nota_legal.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main(){
-  printf("\n\n=============================\n\n");
-  printf("      N O T A  L E G A L      \n");
-  printf("\n=============================\n\n");
-printf("%sProduto         Qtd Valor Unit""\n");
-  printf("%sCamiseta %f2 %i39.90\n");  
-  
+  printf("%s\n", "==============================");
+  printf("%25s\n", "N O T A   L E G A L");
+  printf("%s\n", "==============================");
+  printf("%s\n", "Produto         Qtd Valor Unit");
+  printf("%s %10.3i %10.2f\n", "Camiseta", 002, 39.99);
+  printf("%s %13.3i %10.2f\n", "Calca", 001, 89.90);
+  printf("%s %7.3i %10.2f\n", "Meia Social", 003, 19.99 );
+   printf("%s\n", "==============================");
+   printf("%s %23.2f\n", "Total:", 229.85);
   return 0;
 }
