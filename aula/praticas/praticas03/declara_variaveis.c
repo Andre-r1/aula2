868333c5158cd08540b88ecde289b54d21505fa9
Andre-r1 <andrexpsouza@gmail.com>
2024-08-28 13:57:09 +0000

    pratica03-1

---
+++ aula/praticas/praticas03/declara_variaveis.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+int main(){
+  int idade = 30;
+  printf("Idade: %i\n", idade);
+  float altura = 1.92f;
+  printf("Altura: %.2f\n", altura);
+  char tecla ='A';
+  printf("Tecla: %c\n", tecla);
+double numero_e = 2.7182818284;
+  printf("Numero de Euler: %.10f\n", numero_e);
+  
+  
+  return 0;
+}
